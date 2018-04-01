dca389892d3778736ae16ed6ca7ba1c623919179
JamesAndrew <jandrewshields@gmail.com>
2018-04-01 17:29:45 -0600

    fuuuuuuuuuuuuuuckkkkkkkk

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ def checkClientMessages():
 
 def election():
     print("election started")
-    s.curTerm = s.curTerm + 1
+    s.curTerm = int(s.curTerm) + 1
     s.votedFor = s.name
     s.start_timer()
     s.sendRequestVote(s.curTerm,s.getName(),1,1)
